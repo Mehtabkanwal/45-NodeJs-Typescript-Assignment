681afce38fc4636c9c0b7f536c7ad0897a111b9f
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-05 14:37:09 +0500

    first assignment

---
+++ Q10/Q10.js
@@ -0,0 +1,12 @@
+"use strict";
+// Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t have anything specific to write because your programs are too simple at this point, just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.
+console.log('PROGRAM 1');
+console.log('Kanwal bilal', new Date());
+console.log('this is a simple way to write a famous person quotation');
+console.log(`Albert Einstein once said,"A person who never made a mistake never tried anything new."`);
+let famous_person = 'Albert Einstein';
+let message = `"A person who never made a mistake never tried anything new.”`;
+console.log('PROGRAM 2');
+console.log('kanwal bilal', new Date());
+console.log('In this method we put the authors name & his quote in a variable and then print it ');
+console.log(`${famous_person} once said, ${message}`);
---
+++ Q12/Q12.js
@@ -0,0 +1,6 @@
+"use strict";
+// Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name, print a message to them. The text of each message should be the same, but each message should be personalized with the person’s name.
+let names = ['Sundus', 'Nabila', 'Fatima', 'Sumaira', 'Humaira'];
+for (let name of names) {
+    console.log(`Hello ${name} You are my best friend`);
+}
