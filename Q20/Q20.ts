80b5f7280988cc67cf78fb119bfb2e010ba2b29a
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-08 13:10:50 +0500

    first assignment

---
+++ Q20/Q20.ts
@@ -0,0 +1,10 @@
+// 20.Think of something you could store in a array. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items.
+
+
+let citiesNames = ["karachi" , "Peshawar" , "Naran" , "Kaghan" , "Kashmir" , "Islamabad"];
+let mountainNames = ["JABAL E REHMAT" , "JABAL E NOOR" , "JABAL E SOUR"];
+let countriesNames = ["MAKKAH" , "MADINA" , "BAGHDAD"];
+
+console.log(citiesNames);
+console.log(countriesNames);
+console.log(mountainNames);
