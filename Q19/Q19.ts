e8d07aa80ccce3179dce64f30a1617715dd5ddca
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-08 13:10:05 +0500

    first assignmenr

---
+++ Q19/Q19.ts
@@ -0,0 +1,6 @@
+// 19.Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+
+let guestList :string[] =['Humaira' , 'Noman' , 'Irfan'];
+
+console.log (`I am inviting ${guestList.length} on dinner.`);
