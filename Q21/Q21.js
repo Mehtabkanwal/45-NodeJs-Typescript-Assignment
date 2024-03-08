dd073f90ce22868d84c6dbb3253f29da720338fd
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-08 13:11:31 +0500

    first assignment

---
+++ Q21/Q21.js
@@ -0,0 +1,20 @@
+"use strict";
+// 1.  think  of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.
+let studentsData = {
+    name: "Ahmed",
+    age: 15,
+    rollNo: 101,
+    marksSecured: 960,
+    status: "Passed",
+    gender: "Male",
+};
+let studentsData2 = {
+    name: "Safa",
+    age: 12,
+    rollNo: 102,
+    marksSecured: 980,
+    status: "Passed",
+    gender: "Female",
+};
+console.log(`Student1 Data:${studentsData.name} , ${studentsData.age} , ${studentsData.gender} , ${studentsData.rollNo} , ${studentsData.marksSecured} , ${studentsData.status}`);
+console.log(`Student2 Data:${studentsData.name} , ${studentsData.age} , ${studentsData.gender} , ${studentsData.rollNo} , ${studentsData.marksSecured} , ${studentsData.status}`);
