c5568c6d763a511616bc52a5325552df74bad284
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-09 16:05:46 +0500

    first assignment

---
+++ Q23/Q23.js
@@ -0,0 +1,37 @@
+"use strict";
+// 3. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// let car = 'subaru';
+// console.log("Is car == 'subaru'? I predict True.")
+// console.log(car == 'subaru')
+// • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car1 = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car1 == 'subaru');
+let car = 'civic';
+console.log("Is car == 'civic'? I predict True.");
+console.log(car == 'toyota');
+let age = 18;
+console.log("Is age == 18 ? I predict False.");
+console.log(age == 17);
+let fname = 'Ahmed';
+console.log("Is fname == 'Ahmed'? I predict True.");
+console.log(fname == 'Ahmed');
+let marksObtained = 1098;
+console.log("Is marksObtained == 1098? I predict False.");
+console.log(marksObtained == 1000);
+let car2 = 'Audi';
+console.log("Is car2 == 'Audi'? I predict True.");
+console.log(car == 'Audi');
+let ageStudent = 18;
+console.log("Is age == 18? I predict False.");
+console.log(ageStudent == 15);
+let car3 = 'Alto';
+console.log("Is car == 'Alto'? I predict True.");
+console.log(car3 == 'Alto');
+let car4 = 'civic';
+console.log("Is car == 'civic'? I predict False.");
+console.log(car4 == 'Audi');
+let lname = 'Paatwala';
+console.log("Is lastname == 'Paatwala'? I predict True.");
+console.log(lname == 'Paatwala');
