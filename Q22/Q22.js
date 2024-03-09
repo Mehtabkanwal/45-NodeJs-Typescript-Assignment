8d608be4cc97226e8304642ae177edd70e1bc194
Mehtab kanwal <mehtab.kanwal431@gmail.com>
2024-03-09 16:05:09 +0500

    first assignment

---
+++ Q22/Q22.js
@@ -0,0 +1,6 @@
+"use strict";
+// 2. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing the program.
+let arr = ["Ahmed", "Safa", "Bilal", "Kanwal"];
+console.log(arr[4]);
+//correcting the error by accessing a valid index
+arr[3] = "kanwal";
